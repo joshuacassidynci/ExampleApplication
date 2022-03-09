6fefca98690946379a08e5ac1eaaf3770518e519
joshuacassidy <joshcassidy79@gmail.com>
2022-03-09 18:18:46 +0000

    added docker

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd ExampleApplication/
 docker rm $(docker stop $(docker ps -a -q --filter ancestor=node_app --format="{{.ID}}"))
 docker build -t node_app .
 docker run -p 8443:8443 -d node_app
