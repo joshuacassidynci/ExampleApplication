833236e827572935f5235236211b1105c3f0efc8
joshuacassidy <joshcassidy79@gmail.com>
2022-03-09 22:43:39 +0000

    added docker

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
-cd ExampleApplication/
-npm install
 CURRENT_INSTANCE=$(sudo docker ps -a -q --filter ancestor=node_app --format="{{.ID}}")
 
 if [ "$CURRENT_INSTANCE" ]
 then
   sudo docker rm $(sudo docker stop $CURRENT_INSTANCE)
 fi
-
+sudo docker pull joshuacassidynci/exampleapp
+CONTAINER_EXISTS=$(sudo docker ps -a | grep node_app)
+if [ "$CURRENT_INSTANCE" ]
+then
+  sudo docker rm node_app
+fi
+sudo docker create --name node_app joshuacassidynci/exampleapp
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
-
-sudo docker build -t node_app .
+sudo docker cp ./privatekey.pem node_app:/privatekey.pem
+sudo docker cp ./server.crt node_app:/server.crt
 sudo docker run -p 8443:8443 -d node_app
