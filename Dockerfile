40c88c5f5f919c1eb1a5f5dd5d4a6f0523a52a06
joshuacassidy <joshcassidy79@gmail.com>
2022-03-09 18:17:32 +0000

    added docker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:12.18.1
+ENV NODE_ENV=production
+
+WORKDIR /app
+
+COPY ["package.json", "package-lock.json*", "./"]
+
+RUN npm install --production
+RUN echo $PRIVATE_KEY > privatekey.pem
+RUN echo $SERVER > server.crt
+
+COPY . .
+
+CMD [ "npm", "start" ]
--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,4 @@
-#!/usr/bin/env bash 
-sudo apt update && sudo apt install nodejs npm
-# Install pm2 which is a production process manager for Node.js with a built-in load balancer.
-sudo npm install -g pm2
-# stop any instance of our application running currently
-pm2 stop example_app
-# change directory into folder where application is downloaded
-cd ExampleApplication/
-# Install application dependencies
-npm install
-echo $PRIVATE_KEY > privatekey.pem
-echo $SERVER > server.crt
-# Start the application with the process name example_app using pm2
-pm2 start ./bin/www --name example_app
+#!/usr/bin/env bash
+docker rm $(docker stop $(docker ps -a -q --filter ancestor=node_app --format="{{.ID}}"))
+docker build -t node_app .
+docker run -p 8443:8443 -d node_app
