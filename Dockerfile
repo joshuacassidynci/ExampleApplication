a3bfe64af455dbc909af54157b67d2819628b088
joshuacassidy <joshcassidy79@gmail.com>
2022-03-09 20:57:26 +0000

    updated docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM circleci/node:10.16.3
 ENV NODE_ENV=production
 COPY . .
-RUN echo $PRIVATE_KEY > privatekey.pem
-RUN echo $SERVER > server.crt
 CMD [ "npm", "start" ]
--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ if [ -z "$CURRENT_INSTANCE" ]
 then
       sudo docker rm $(sudo docker stop $(CURRENT_INSTANCE))
 fi
-
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 sudo docker build -t node_app .
 sudo docker run -p 8443:8443 -d node_app
 
